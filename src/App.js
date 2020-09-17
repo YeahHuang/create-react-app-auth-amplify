5aa97b669772ee0b454ce23edcabf104babe03bd
YeahHuang <yeah.huangx@foxmail.com>
2020-09-16 23:29:40 -0400

    update using video in AWS S3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,64 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { withAuthenticator } from 'aws-amplify-react'
-import Amplify, { Auth } from 'aws-amplify';
+import { withAuthenticator } from 'aws-amplify-react'; // For user-authentication logIn, logOut
+import Amplify, { Auth } from 'aws-amplify';        //Auth might be deleted
 import aws_exports from './aws-exports';
+
+import videojs from "video.js";
+import "video.js/dist/video-js.css";
+import awsvideoconfig from "./aws-video-exports";
+
 Amplify.configure(aws_exports);
+
+//class VideoPlayer is copied and edited from https://codingcat.dev/lessons/aws-amplify-video/
+class VideoPlayer extends React.Component {
+  componentDidMount() {
+    this.player = videojs(this.videoNode, this.props);
+  }
+
+  componentWillUnmount() {
+    if (this.player) {
+      this.player.dispose();
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div data-vjs-player>
+          <video
+            ref={(node) => {
+              this.videoNode = node;
+            }}
+            className="video-js"
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+const videoJsOptions = {
+  autoplay: true,
+  controls: true,
+  sources: [
+    {
+      src: awsvideoconfig.awsOutputLiveLL,
+    },
+  ],
+};
+
+const videoOnDemandJsOptions = {
+  autoplay: true,
+  controls: true,
+  sources: [
+    {
+      src: `https://${awsvideoconfig.awsOutputVideo}/jump/jump.m3u8`,
+    },
+  ],
+};
+
 
 class App extends Component {
   render() {
@@ -24,9 +78,12 @@ class App extends Component {
             Learn React
           </a>
         </header>
-    <video width="750" height="500" controls >
-      <source src="/Videos/jump.mp4" type="video/mp4"/>
-     </video>
+    // Option 1: static video player
+    // <video width="750" height="500" controls >
+    //   <source src="/Videos/jump.mp4" type="video/mp4"/>
+    //  </video>
+        <VideoPlayer {...videoJsOptions} />
+        <VideoPlayer {...videoOnDemandJsOptions} />
       </div>
     );
   }
